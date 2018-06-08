a57d3e8363587dc72dfaa8577fa1a24f5f3c8131
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-06-08 11:23:38 -0700

    add oozie configs

---
+++ recipes/oozie.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook:: hdp-chef
+# Recipe:: oozie
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+
+# install/configure kafka
+include_recipe "#{cookbook_name}::oozie_user"
+include_recipe "#{cookbook_name}::oozie_lib"
--- recipes/oozie_lib.rb
+++ recipes/oozie_lib.rb
@@ -7,7 +7,7 @@
 
 # create oozie lib dir
 directory "create_/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie" do
-  path "/usr/hdp/#{node['hw']['hdp']['version_full']}"/oozie
+  path "/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie"
   recursive true
   action :create
 end
@@ -21,52 +21,48 @@ directory "create_/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib"
 end
 
 # applications - itterate though applications
-node['hw']['oozie']['lib'].each do |app|
+node['hw']['oozie']['lib'].each do |app_name,app|
 
   # create app directory
-  directory "create_/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app}" do
-    path "/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app}"
+  directory "create_/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app_name}" do
+    path "/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app_name}"
     owner node['hw']['oozie']['user']['name']
     group node['hw']['hadoop']['common']['group']['name']
     action :create
   end
 
   # jars - remove undesired jars
-  node['hw']['oozie']['lib'][app]['jars_remove'].each do |jar|
-    file "delete_/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app}/#{jar}" do
-      path "/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app}/#{jar}"
+  node['hw']['oozie']['lib'][app_name]['jars_remove'].each do |jar_name|
+    file "delete_/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app_name}/#{jar_name}" do
+      path "/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app_name}/#{jar_name}"
       action :delete
     end
   end
 
   # jars - download desired jars
-  node['hw']['oozie']['lib'][app]['jars_install'].each do |jar|
-    node['hw']['oozie']['lib'][app]['jars_install'][jar].each do |name, source, hash|
-
-      # jars - download desired jars
-      remote_file "download_/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app}/#{jar}" do
-        source "#{source}"
-        checksum "#{checksum}"
-        path "install_/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app}/#{name}"
-        recursive true
-        owner node['hw']['oozie']['user']['name']
-        group node['hw']['hadoop']['common']['group']['name']
-      end
+  node['hw']['oozie']['lib'][app_name]['jars_install'].each do |jar_name,jar|
+    # jars - download desired jars
+    remote_file "download_/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app_name}/#{jar['name']}" do
+      source "#{jar['source']}"
+      checksum "#{jar['checksum']}"
+      path "/usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app_name}/#{jar['name']}"
+      owner node['hw']['oozie']['user']['name']
+      group node['hw']['hadoop']['common']['group']['name']
     end
   end
 end
   
 # applications - itterate though applications
-node['hw']['oozie']['lib'].each do |app|
+node['hw']['oozie']['lib'].each do |app_name,app|
 
   # copy jars to hdfs
-  bash "copy_oozie_#{app}jars_to_hdfs" do
+  bash "copy_oozie_#{app_name}jars_to_hdfs" do
     code <<-EOF
-      hdfs dfs -put /usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app}/*.jar /user/oozie/share/lib/lib_*/#{app}/
-      hdfs dfs -chown -R #{node['hw']['oozie']['user']['name']}:#{node['hw']['hadoop']['common']['user']['name']}/user/oozie/share/lib/lib_*/#{app}
-      hdfs dfs -chmod 644 /user/oozie/share/lib/lib_*/#{app}/*.jar
+      hdfs dfs -put /usr/hdp/#{node['hw']['hdp']['version_full']}/oozie/share/lib/#{app_name}/*.jar /user/oozie/share/lib/lib_*/#{app_name}/
+      hdfs dfs -chown -R #{node['hw']['oozie']['user']['name']}:#{node['hw']['hadoop']['common']['group']['name']} /user/oozie/share/lib/lib_*/#{app_name}
+      hdfs dfs -chmod 644 /user/oozie/share/lib/lib_*/#{app_name}/*.jar
     EOF
-    user node['hw']['hadoop']['common']['user']['name']  
+    user node['hw']['hadoop']['hdfs']['user']['name']  
     action :nothing
   end
 end
