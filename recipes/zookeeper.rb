99bb884207af514ff8a240b4ba58d8aaab54c4da
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-06-12 13:35:07 -0700

    add zookeeper configs

---
+++ recipes/zookeeper.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: hdp-chef
+# Recipe:: zookeeper
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+
+# install/configure kafka
+include_recipe "#{cookbook_name}::zookeeper_user"
