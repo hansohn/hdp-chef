3b9dfe8842e288d49de651daa9b29516c35171f0
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-06-15 16:11:00 -0700

    split out cluster initialization

--- recipes/hdp.rb
+++ recipes/hdp.rb
@@ -4,11 +4,10 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-# install ambari
-include_recipe 'ambari-chef::ambari_server_prerequisites'
-include_recipe 'ambari-chef::ambari_server'
+# install hdp repo
+include_recipe "#{cookbook_name}::repo_hdp"
 
-# install blueprint applications
+# config hdp applications
 include_recipe "#{cookbook_name}::ambari_infra"
 include_recipe "#{cookbook_name}::ambari_metrics"
 include_recipe "#{cookbook_name}::hadoop_hdfs"
@@ -17,7 +16,3 @@ include_recipe "#{cookbook_name}::kafka"
 include_recipe "#{cookbook_name}::logsearch"
 include_recipe "#{cookbook_name}::oozie"
 include_recipe "#{cookbook_name}::zookeeper"
-
-# install hortonworks data platform
-include_recipe "#{cookbook_name}::repo_hdp"
-include_recipe "#{cookbook_name}::hdp_cluster"
