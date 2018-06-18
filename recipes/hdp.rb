5f854938c0cf8530b6f77a9aa4032c513bb261cf
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-06-18 00:10:03 -0700

    update recipe list

--- recipes/hdp.rb
+++ recipes/hdp.rb
@@ -8,11 +8,11 @@
 include_recipe "#{cookbook_name}::repo_hdp"
 
 # config hdp applications
-include_recipe "#{cookbook_name}::ambari_infra"
-include_recipe "#{cookbook_name}::ambari_metrics"
 include_recipe "#{cookbook_name}::hadoop_hdfs"
 include_recipe "#{cookbook_name}::hadoop_yarn"
 include_recipe "#{cookbook_name}::kafka"
-include_recipe "#{cookbook_name}::logsearch"
 include_recipe "#{cookbook_name}::oozie"
 include_recipe "#{cookbook_name}::zookeeper"
+
+# add hdp cluster files
+include_recipe "#{cookbook_name}::hdp_cluster"
