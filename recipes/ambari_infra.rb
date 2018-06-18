05564b9c40f6bcf4306690901da4de89610ba3a2
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-06-18 00:08:19 -0700

    update attributes

--- recipes/ambari_infra.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: hdp-chef
-# Recipe:: ambari_infra
-#
-# Copyright (c) 2017 The Authors, All Rights Reserved.
-
-# install/configure ambari infra
-include_recipe "#{cookbook_name}::ambari_infra_user"
-include_recipe "#{cookbook_name}::ambari_infra_config"
--- recipes/ambari_metrics.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: hdp-chef
-# Recipe:: ambari_metrics
-#
-# Copyright (c) 2017 The Authors, All Rights Reserved.
-
-# install/configure ambari metrics
-include_recipe "#{cookbook_name}::ambari_metrics_user"
-include_recipe "#{cookbook_name}::ambari_metrics_config"
