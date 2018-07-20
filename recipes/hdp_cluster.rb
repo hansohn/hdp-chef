4418f084f87e1911fbca32d343bced9940377105
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-07-20 06:27:45 -0700

    update licenses

--- recipes/hdp_cluster.rb
+++ recipes/hdp_cluster.rb
@@ -2,12 +2,49 @@
 # Cookbook Name:: hdp-chef
 # Recipe:: hdp_cluster
 #
-# Copyright (c) 2017 The Authors, All Rights Reserved.
+# The MIT License (MIT)
+#
+# Copyright:: 2018, Ryan Hansohn
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 # hdp-cluster: create clusters directory
 directory 'make_/var/lib/ambari-clusters' do
   path '/var/lib/ambari-clusters'
   recursive true
+  owner node['hw']['ambari']['server']['user']['name']
+  group 'root'
+end
+
+# hdp-cluster: add version definition file to cluster dir
+remote_file "create_/var/lib/ambari-clusters/HDP-#{node['hw']['hdp']['version_full']}.xml" do
+  source node['hw']['hdp']['vdf']
+  path "/var/lib/ambari-clusters/HDP-#{node['hw']['hdp']['version_full']}.xml"
+  owner node['hw']['ambari']['server']['user']['name']
+  group 'root'
+  action :create_if_missing
+end
+
+# hdp-cluster: add version definition post to cluster dir
+template "create_/var/lib/ambari-clusters/#{node['hw']['cluster']['version_definition_file']}" do
+  path "/var/lib/ambari-clusters/#{node['hw']['cluster']['version_definition_file']}"
+  source 'version_definition_file.json.erb'
   owner node['hw']['ambari']['server']['user']['name']
   group 'root'
 end
