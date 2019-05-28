ce9b5aa87d0137b172225657be423d14bb2ba326
Ryan Hansohn <hansohn@users.noreply.github.com>
2019-05-27 23:17:02 -0700

    bunp version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@imnorobot.com'
 license 'MIT'
 description 'Installs/Configures hdp-chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.1.0'
+version '2.2.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 source_url 'https://github.com/hansohn/hdp-chef' if respond_to?(:source_url)
 issues_url 'https://github.com/hansohn/hdp-chef/issues' if respond_to?(:issues_url)
